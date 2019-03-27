4ee0666041d3d793705d0dd035f1576954323ac0
janie <janie.ondracek@gmail.com>
2019-03-27 20:47:30 +0100

    ups to OT code

--- OT_Analysis/janieAnalysis_ChickenOT/chickenOTAnalysisScripts.m
+++ OT_Analysis/janieAnalysis_ChickenOT/chickenOTAnalysisScripts.m
@@ -11,15 +11,15 @@ close all
 
 %% Create Chicken Analysis Object
 
-experiment = 4; %efc
-recSession = 5; %sc
+experiment = 6; %efc
+recSession = 1; %sFigSaveNamec
 
 C_OBJ = chicken_OT_analysis_OBJ(experiment, recSession);
 
 % %% Stimulus Protocol
 % Stim Protocol: (1) HRTF; (2) Tuning; (3) IID; (4) ITD; (5) WN
 
-audSelInd = 1; % This is the index, not the stim number!!!
+audSelInd = 6; % SpikesThis is the index, spikesnot the stim number!!!
 
 selection = C_OBJ.RS_INFO.ResultDirName{audSelInd};
 disp(selection)
@@ -29,7 +29,7 @@ disp(selection)
 disp('Loading Saved Object...')
 
 audStimDir = C_OBJ.RS_INFO.ResultDirName{audSelInd};
-objFile = 'C_OBJ.mat';
+objFile = 'C_OBJ.mat';printRaster_IID_Ver1
 objPath = [C_OBJ.PATHS.OT_Data_Path C_OBJ.INFO.expDir C_OBJ.PATHS.dirD audStimDir C_OBJ.PATHS.dirD '__Spikes' C_OBJ.PATHS.dirD objFile];
 load(objPath);
 disp(['Loaded: ' objPath])
@@ -51,10 +51,10 @@ axis tight
 %% Define spike threhsold and save Figure
 
 %SpkThresh = 0.4;
-SpkThresh = -0.89;
+SpkThresh = 0.11;
 %setThreshAndPrintFig(C_OBJ, SpkThresh)
 
-% Print spikes
+% Print spikes7
 ylim([-1 1])
 
 disp('Printing Plot')
@@ -64,21 +64,22 @@ FigSaveName = [C_OBJ.PATHS.spkSavePath '_spikesResp'];
             
 saveName = C_OBJ.PATHS.audStimDir;
 %mediaSavePath = ['/home/janie/Data/TUM/OTAnalysis/allITDJanie/' saveName '_spikesResp'];
-mediaSavePath = ['/home/janie/Data/TUM/OTAnalysis/allWNsJanie/' saveName '_spikesResp'];
-            
+%mediaSavePath = ['/home/janie/Data/TUM/OTAnalysis/allWNsJanie/' saveName '_spikesResp'];
+mediaSavePath = ['/home/janie/Data/TUM/OTAnalysis/allIIDJanie/' saveName '_spikesResp'];
+
 plotpos = [0 0 15 10];
 %print_in_A4(0, FigSaveName, export_to, 0, plotpos);
 %print_in_A4(0, FigSaveName, '-depsc', 0, plotpos);
 %print_in_A4(0, FigSaveName, '-djpeg', 0, plotpos);
-print_in_A4(0, mediaSavePath, '-djpeg', 0, plotpos);
+print_in_A4(30, mediaSavePath, '-djpeg', 0, plotpos);
 
-% Run Spike Sort - Make sure UMS2K is on the path
+% Run Spike Sort - Make sure UMS2K 5is on the path6
 
 clear('spikes')
 spikes = ss_default_params(C_OBJ.Fs, 'thresh', SpkThresh );
 spikes = ss_detect(C_OBJ.O_STIMS.allEpochData, spikes);
 
-%spikes = ss_align(spikes);
+%spikes = ss_align(spikes);3
 spikes = ss_kmeans(spikes);
 spikes = ss_energy(spikes);
 spikes = ss_aggregate(spikes);
@@ -87,7 +88,7 @@ spikes = ss_aggregate(spikes);
 
 splitmerge_tool(spikes)
 
-%% Print Spike Cluster
+%% Print Spike Cluster5
 
 
 %% Outlier
@@ -95,7 +96,7 @@ outlier_tool(spikes)
 
 %% Define Cluster of Interest
 
-clustOfInterest = 1; 
+clustOfInterest = 11; 
 nSpikesInCluster = numel(find(spikes.assigns == clustOfInterest));
 disp(['nSpikes = ' num2str(nSpikesInCluster)]);
 
@@ -124,10 +125,17 @@ save(cobjSaveName , 'C_OBJ', '-v7.3')
 % save to special directory
 
 %objSaveName = ['/home/janie/Data/TUM/OTAnalysis/allITDJanie/allObjs/' C_OBJ.PATHS.audStimDir '_C_OBJ.mat']; 
-objSaveName = ['/home/janie/Data/TUM/OTAnalysis/allWNsJanie/allObjs/' C_OBJ.PATHS.audStimDir '_C_OBJ.mat']; 
+%objSaveName = ['/home/janie/Data/TUM/OTAnalysis/allWNsJanie/allObjs/' C_OBJ.PATHS.audStimDir '_C_OBJ.mat']; 
+objSaveName = ['/home/janie/Data/TUM/OTAnalysis/allIIDJanie/allObjs/' C_OBJ.PATHS.audStimDir '_C_OBJ.mat']; 
+
 save(objSaveName , 'C_OBJ', '-v7.3')
 disp(['Saved: '  objSaveName])
 
+%
+printRaster_IID_Ver1(C_OBJ, 1) % IID
+
+
+%%
 % WN with raster, psth and z score
 printRaster_WN_Ver2(C_OBJ)
 
