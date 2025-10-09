d438464d7edbd28b2cf5873f7356b9f54d7d8342
ondracej <janie.ondracek@gmail.com>
2025-10-09 13:48:06 +0200

    updates to spike sorting for OT

---
+++ OT_Analysis/organizePlexonSpikeTimesIntoStimRepetitons_Wrapper.m
@@ -0,0 +1,31 @@
+function [] = organizePlexonSpikeTimesIntoStimRepetitons_Wrapper()
+
+%SortedSpikeFile_Dir = 'X:\Janie-OT-MLD\PlexonData-HRTF_2025\';
+SortedSpikeFile_Dir = 'X:\Janie-OT-MLD\PlexonData-WN_2025\';
+StimType = 5; %HRTF 3 (IID), 4 (ITD), 5 (WN)
+
+sortedFile_txt = '*__Sorted.mat';
+sortedFiles = dir(fullfile(SortedSpikeFile_Dir, sortedFile_txt));
+sortedFiles = {sortedFiles .name}';
+nSortedFiles = numel(sortedFiles);
+
+for q = 1:nSortedFiles
+    % Load sorted data
+    load([SortedSpikeFile_Dir sortedFiles{q}])
+    % Load accompanying INFO
+    InfoFile = [sortedFiles{q}(1:end-16) 'INFO.mat'];
+    load([SortedSpikeFile_Dir InfoFile])
+    
+    fileNameBase = sortedFiles{q}(1:end-16);
+    
+    organizePlexonSpikeTimesIntoStimRepetitions(INFO, Channel01, fileNameBase, StimType)
+end
+
+
+
+
+
+
+
+
+end
